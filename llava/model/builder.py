541372bd0a1733a861370d91beca146b228da19f
hasanibnarif <hasanarif@vt.edu>
2024-05-26 19:26:21 -0400

    llava next

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -25,6 +25,7 @@ from llava.utils import rank0_print
 
 
 def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=False, device_map="auto", attn_implementation="flash_attention_2", customized_config=None, **kwargs):
+    print(" ################################## Loading llava model using the latest llava-next repo #################################")
     kwargs = {"device_map": device_map}
 
     if load_8bit:
